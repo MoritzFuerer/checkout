d50a3affccc0ca671d05e22d02c1cddbe2465c62
fueremor <fueremor@students.zhaw.ch>
2022-10-31 15:04:27 +0100

    2

--- src/main/java/ch/zhaw/checkout/checkout/model/Product.java
+++ src/main/java/ch/zhaw/checkout/checkout/model/Product.java
@@ -2,16 +2,26 @@ package ch.zhaw.checkout.checkout.model;
 
 
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
 
 
 public class Product {
     
     
     private String id;
+    @NonNull
     private String name;
+    @NonNull
     private String productGroup;
+
     private double price;
 
 }
