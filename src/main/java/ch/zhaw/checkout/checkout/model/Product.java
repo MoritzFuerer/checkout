c7cd769b213a4d17dc4ad75be07d7ab694b339ed
fueremor <fueremor@students.zhaw.ch>
2022-10-31 14:53:11 +0100

    1

---
+++ src/main/java/ch/zhaw/checkout/checkout/model/Product.java
@@ -0,0 +1,17 @@
+package ch.zhaw.checkout.checkout.model;
+
+
+
+
+
+
+
+public class Product {
+    
+    
+    private String id;
+    private String name;
+    private String productGroup;
+    private double price;
+
+}
