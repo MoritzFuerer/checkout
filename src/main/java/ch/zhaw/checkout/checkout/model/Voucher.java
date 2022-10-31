c01248183e47b29be417c109c98b0f07bcad78f0
fueremor <fueremor@students.zhaw.ch>
2022-10-31 17:58:48 +0100

    6

--- src/main/java/ch/zhaw/checkout/checkout/model/Voucher.java
+++ src/main/java/ch/zhaw/checkout/checkout/model/Voucher.java
@@ -2,6 +2,9 @@ package ch.zhaw.checkout.checkout.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import ch.zhaw.checkout.checkout.model.FiveBuckesVoucher;
+import ch.zhaw.checkout.checkout.model.Product;
+
 import java.util.List;
 
 public @interface Voucher {
